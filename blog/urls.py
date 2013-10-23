7e075ba0a26337c29acbdff25c6bc88ded18936a
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-23 16:25:03 +0200

    Added color variables, worked on grid

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('blog.views',
         url(r'^$', 'home', name='home'),
-        #url('r^posts/(?P<slug>[-_\w+])', 'show_post', name='show_post'),
+        url('r^posts/(?P<slug>[-_\w+])', 'show_post', name='show_post'),
         )
