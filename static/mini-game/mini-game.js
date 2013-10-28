e6019cbb62d6ffed46045b58c3c19c47821d876e
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-28 14:57:13 +0100

    Mini-game work: flipped sprites, added AJAX loading

---
+++ static/mini-game/mini-game.js
@@ -0,0 +1,63 @@
+"use strict";
+
+(function($) {
+  $(window).load(function() {
+    // Mini-game
+    (function(canvasId, _) {
+      // Manifest: what to load
+      var resources = (function(data) {
+        for (var key in data) {
+          data[key] = django.static(data[key]);
+        }
+        return data;
+      }) ({
+        'ganondorfSS': 'mini-game/spritesheets/ganondorf-lr.png',
+        'ganondorfSSdata': 'mini-game/spritesheets/ganondorf-lr.json'
+      });
+
+      // Main stage
+      var stage = new _.Stage(canvasId);
+
+      // Ganondorf spritesheet
+      var ganondorfSSdata = {
+        framerate: 8,
+        images: [resources['ganondorfSS']],
+        animations: {
+          walk: { frames: [5, 4, 3, 2, 1] },
+          run: {
+            frames: [5, 4, 3, 2, 1],
+            speed: 2
+          }
+        }
+      };
+      $.ajax(resources['ganondorfSSdata'], {
+        success: function(data) {
+          ganondorfSSdata.frames = data.frames;
+        }, async: false
+      });
+      var ganondorfSS = new _.SpriteSheet(ganondorfSSdata);
+
+      // Ganondorf instance
+      // TODO
+      var ganondorf = new _.Sprite(ganondorfSS, 'walk');
+
+      ganondorf.on('tick', function(evt) {
+        ganondorf.x += 1;
+      });
+      stage.addChild(ganondorf);
+
+      // Game loop setup
+      _.Ticker.timingMode = _.Ticker.RAF;
+      _.Ticker.on('tick', function(evt) {
+        stage.update(evt);
+      });
+    }) ('mini-game-canvas', createjs);
+
+    // UI integration
+    $('#mini-game-toggle').on('click', function() {
+      var _ = createjs;
+      _.Ticker.setPaused(!_.Ticker.getPaused());
+      $('#mini-game-canvas').parents().first().slideToggle(500);
+    });
+  });
+}) (jQuery);
