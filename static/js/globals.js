f4aa9d9f39bb3689253264af44cc916efd0d0f01
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-10-28 17:18:31 +0100

    Continuing on spritesheet switch

---
+++ static/js/globals.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function assert(expr, obj) {
+  if (!expr) {
+    throw obj || "Assertion failed";
+  }
+}
+
+function staticURI(url, encode) {
+  if (encode == undefined) { encode = false; }
+  if (url[0] != '/') { url = '/' + url };
+  url = '/static' + url;
+  if (encode) { url = encodeURI(url); }
+  return url;
+}
--- static/mini-game/mini-game.js
+++ static/mini-game/mini-game.js
@@ -4,50 +4,134 @@
   $(window).load(function() {
     // Mini-game
     (function(canvasId, _) {
-      // Manifest: what to load
-      var resources = (function(data) {
-        for (var key in data) {
-          data[key] = django.static(data[key]);
+      // Spritesheet data/images to load
+      var spriteSheets = (function(targets) {
+        var data = {};
+
+        for (var i = 0; i < targets.length; i++) {
+          var target = targets[i],
+            base = staticURI('mini-game/spritesheets/' + target);
+          data[target] = {};
+          data[target].image = base + '.png';
+          data[target].data = base + '.json';
         }
+
         return data;
-      }) ({
-        'ganondorfSS': 'mini-game/spritesheets/ganondorf.png',
-        'ganondorfSSdata': 'mini-game/spritesheets/ganondorf.json'
-      });
+      }) (['ganondorf-left', 'ganondorf-right']);
+
+      // Keyboard event dispatcher (use to bind key events)
+      var keyboard = new _.EventDispatcher();
 
       // Main stage
       var stage = new _.Stage(canvasId);
+      //...helpers
+      stage.height = stage.canvas.height;
+      stage.width = stage.canvas.width;
+      //...center origin for stage
+      stage.regX = -stage.width/2;
+      stage.regY = -stage.height/2;
 
-      // Ganondorf spritesheet
-      var ganondorfSSdata = {
+      // Ganondorf spritesheets
+      var ganondorfSS = (function(data) {
+        var res = [], ss = ['left', 'right'];
+
+        for (var i = 0; i < ss.length; i++) {
+          var spriteSheet = spriteSheets['ganondorf-' + ss[i]];
+
+          // Images to load for spritesheet
+          data.images = [spriteSheet.image];
+
+          // Load spritesheet JSON data
+          $.ajax({
+            url: spriteSheet.data,
+            success: function(jsonData) {
+              data.frames = jsonData.frames;
+            }, async: false
+          });
+
+          res[ss[i]] = new _.SpriteSheet(data);
+        }
+
+        return res;
+      }) ({
         framerate: 8,
-        images: [resources['ganondorfSS']],
         animations: {
-          walk: { frames: [5, 4, 3, 2, 1] },
-          run: {
+          idle: {
+            frames: [2]
+          }, walk: {
+            frames: [5, 4, 3, 2, 1]
+          }, run: {
             frames: [5, 4, 3, 2, 1],
             speed: 2
           }
         }
-      };
-      $.ajax(resources['ganondorfSSdata'], {
-        success: function(data) {
-          ganondorfSSdata.frames = data.frames;
-        }, async: false
       });
-      var ganondorfSS = new _.SpriteSheet(ganondorfSSdata);
 
-      // Ganondorf instance
-      // TODO
-      var ganondorf = new _.Sprite(ganondorfSS, 'walk');
-      ganondorf.on('tick', function(evt) {
-      });
+      // Ganondorf sprite
+      var ganondorf = new _.Sprite(ganondorfSS['left'], 'idle');
       stage.addChild(ganondorf);
+
+      // Ganondorf logic
+      var ganondorfLogic = {
+        state: {
+          direction: 'left',
+          motion: 0,
+          speed: 3
+        }, move: function(direction) {
+          assert(direction == 'left' || direction == 'right');
+          this.state.direction = direction;
+          this.state.motion = (direction == 'left') ? -1 : 1;
+        }
+      };
+
+      // Ganondorf events
+      ganondorf.on('tick', function(evt) {
+        var self = this,
+          bounds = self.getBounds(),
+          state = ganondorfLogic.state;
+
+        // Move sprite registration point to center
+        self.regX = bounds.width/2;
+        self.regY = bounds.height/2;
+
+        // (x, y) coordinates
+        self.x += state.motion*state.speed;
+        self.y = Math.max(Math.min(self.y + 10,
+            stage.height/2 - bounds.height/2),
+          -stage.height/2 + bounds.height/2);
+
+        // Left/Right flip depending on direction
+      });
+      // ...key events
+      keyboard.on('left', function() {
+        ganondorfLogic.move('left');
+      });
+      keyboard.on('right', function() {
+        ganondorfLogic.move('right');
+      });
+      keyboard.on('up', function() {
+        // jump
+      });
 
       // Game loop
       _.Ticker.timingMode = _.Ticker.RAF;
       _.Ticker.on('tick', function(evt) {
         stage.update(evt);
+      });
+
+      // Keyboard events
+      $(document).on('keydown', function(e) {
+        // Don't handle if game is paused
+        if (_.Ticker.getPaused()) { return; }
+
+        if (e.keyCode == 37) { keyboard.dispatchEvent('left'); }
+        else if (e.keyCode == 39) { keyboard.dispatchEvent('right'); }
+        else if (e.keyCode == 38) { keyboard.dispatchEvent('up'); }
+        else if (e.keyCode == 40) { keyboard.dispatchEvent('down'); }
+        else { return true; }
+
+        // Don't handle other keyboard events
+        return false;
       });
     }) ('mini-game-canvas', createjs);
 
