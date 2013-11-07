fbb052c64f6371689df739498d4c34cd8682bc6f
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-11-07 13:04:07 +0100

    Fix font joke

--- static/js/font-joke.js
+++ static/js/font-joke.js
@@ -4,7 +4,7 @@
   $(window).load(function() {
     $('#font-joke').one('click', function() {
       less.modifyVars({ '@base-font': 'Comic Sans Ms' });
-      $(this).text('Sucka').append('&nbsp; <i class="fa fa-smile-o"></i>');
+      $(this).text('Sucka').append('&nbsp; <i class="fe fe-emo-devil"></i>');
     });
   });
 }) (jQuery);
