0c46086e4a3c831c42073b9abcfc38bee654557f
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-12-01 12:11:36 +0100

    Fix small bug with integrity error

--- blog/management/commands/addpost.py
+++ blog/management/commands/addpost.py
@@ -1,36 +1,8 @@
-import os
-import subprocess
-from tempfile import NamedTemporaryFile
 from optparse import make_option
+from django.db import IntegrityError
 from django.core.management.base import BaseCommand, CommandError
 from blog.models import Category, Post
-
-def get_external_content(filename=None, initial_content=''):
-    """
-    Read external content from the user's EDITOR, defaulting to vi. Initial
-    content may be specified by giving a list of lines or a string to write at
-    the beginning of the tmpfile created in the process.
-    Raises an IOError if an error occured.
-    """
-    if filename:
-        fp = open(filename, 'rw')
-    else:
-        fp = NamedTemporaryFile()
-        filename = fp.name
-
-    # write initial content
-    if initial_content:
-        if isinstance(initial_content, (list, tuple)):
-            fp.writelines(initial_content)
-        else:
-            fp.write(initial_content)
-
-    # edit tmpfile
-    editor = os.environ.get('EDITOR', 'vi')
-    if subprocess.call([editor, filename]) != 0:
-        raise IOError('EDITOR exited with a non-zero status')
-
-    return fp.read()
+from _private import get_external_content
 
 class Command(BaseCommand):
     args = '<post_title>'
@@ -45,7 +17,7 @@ class Command(BaseCommand):
     def handle(self, title, *args, **options):
         post = Post(title=title)
 
-        # category
+        # category read by csv
         category_title_list = options['categories'].split(',')
         for category_title in category_title_list:
             if not category_title:
@@ -57,10 +29,14 @@ class Command(BaseCommand):
             except Category.DoesNotExist:
                 raise CommandError('Category "%s" does not exist' % category_title)
 
-        # content
+        # content read from external program
         try:
             post.content = get_external_content()
         except IOError as e:
             raise CommandError(e)
 
-        post.save()
+        # validate and save
+        try:
+            post.save()
+        except IntegrityError as e:
+            print 'Post cannot be created, reason: %s' % e
