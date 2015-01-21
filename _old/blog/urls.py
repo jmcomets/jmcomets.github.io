c37aa2e3919930429fb8616030ea30db45761626
Jean-Marie Comets <jean.marie.comets@gmail.com>
2015-01-21 19:56:51 +0100

    Remove old site data

--- _old/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('blog.views',
-        url(r'^$', 'home', name='home'),
-        url(r'^about/?$', 'about', name='about'),
-        url(r'^posts/new/?$', 'new_post', name='create_post'),
-        url(r'^posts/show/(?P<slug>[-_\w]+)/?$', 'show_post', name='show_post'),
-        )
--- _old/website/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-WSGI config for jmcomets.com project.
-"""
-
-from os import environ as env
-env.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings_no_debug')
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
